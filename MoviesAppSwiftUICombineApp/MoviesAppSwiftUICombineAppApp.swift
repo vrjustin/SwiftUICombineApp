d1038af9a78554b47c6661eae52039fa32a5bbd3
Justin Maronde <myemailisjustin@me.com>
2024-09-10 05:29:22 -0400

    Initial Commit

---
+++ MoviesAppSwiftUICombineApp/MoviesAppSwiftUICombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppSwiftUICombineAppApp.swift
+//  MoviesAppSwiftUICombineApp
+//
+//  Created by Justin Maronde on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppSwiftUICombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
